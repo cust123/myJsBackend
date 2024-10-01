b180cf55f972338c2d89949c8776ec69e97027c5
Muhammad Shafiq <shafiqpesh123@gmail.com>
2024-10-01 14:11:01 +0500

    further changes

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-const port = 3000;
+// const port = 4000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World and welcome to JavaScript Backend!");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.get("/twitter", (req, res) => {
+  res.send("muhammadshafiq");
+});
+app.get("/login", (req, res) => {
+  res.send("<h1>Please Login at Chai aur Code </h1>");
+});
+app.get("/youtube", (req, res) => {
+  res.send("<h2>https://www.youtube.com/@chaiaurcode</h2>");
+});
+app.listen(process.env.PORT, () => {
+  console.log(`welcome to JavaScript Backend!`);
 });
